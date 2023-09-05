e5d159ae0b8ee48003a5da1615963bf6216de9db
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2023-09-05 18:42:46 +0700

    Error handing 2

--- js/errorHandling.js
+++ js/errorHandling.js
@@ -1,4 +1,4 @@
-onerror = (event, source, lineno, colno, error) => {
+window.onerror = (event, source, lineno, colno, error) => {
     navigator.clipboard.writeText(error);
-    alert("An error has occurred. The error message has been copied into your clipboard. Please contact the game author.\n\n--------------------\n\n" + error);
+    alert("An error has occurred. The error message has been copied into your clipboard. Please contact the game author.\n\n--------------------\n\n" + event);
 }
