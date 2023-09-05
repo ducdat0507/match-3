d457c9a7215e1d78700b2bfe055b79bab4095685
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2023-09-05 18:43:31 +0700

    Error handing 3

--- js/errorHandling.js
+++ js/errorHandling.js
@@ -1,4 +1,4 @@
 window.onerror = (event, source, lineno, colno, error) => {
-    navigator.clipboard.writeText(error);
+    navigator.clipboard.writeText(event);
     alert("An error has occurred. The error message has been copied into your clipboard. Please contact the game author.\n\n--------------------\n\n" + event);
 }
